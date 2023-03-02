b41c0d3bb652afc1700929f1ba2a5fd6208a8917
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-03-02 20:56:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const iOSVersion = navigator.userAgent.match(/iPhone OS \d+/);
 
 if (iPhoneModel && iOSVersion) {
   const version = iOSVersion[0].replace(/[^0-9_]/g, "").replace("_", ".");
-  const versionText = `Version iOS ${version}`;
+  const modelText = `Modèle d'iPhone : ${iPhoneModel[0]}`;
+  const versionText = `Version iOS : ${version}`;
+  const infoText = `${modelText} - ${versionText}`;
+  const iphoneInfo = document.getElementById("iphone-info");
+  iphoneInfo.textContent = infoText;
+}
