5bd2e799a7ad94defa593ab621713996bfbe7975
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-03-03 19:23:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
-const carouselImages = document.querySelector(".carousel-images");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
+.carousel {
+  position: relative;
+  width: 100%;
+}
 
-let scrollAmount = 0;
-const scrollStep = 200;
+.carousel-images {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+}
 
-prevButton.addEventListener("click", () => {
-  carouselImages.scrollTo({
-    left: (scrollAmount -= scrollStep),
-    behavior: "smooth"
-  });
-});
+.carousel-images img {
+  flex: 0 0 auto;
+  width: 100%;
+  height: auto;
+  scroll-snap-align: center;
+}
 
-nextButton.addEventListener("click", () => {
-  carouselImages.scrollTo({
-    left: (scrollAmount += scrollStep),
-    behavior: "smooth"
-  });
-});
+.carousel-navigation {
+  position: absolute;
+  bottom: 10px;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+}
+
+.carousel-dots {
+  display: flex;
+}
+
+.carousel-dot {
+  width: 10px;
+  height: 10px;
+  margin: 0 5px;
+  border-radius: 50%;
+  background-color: #ccc;
+  cursor: pointer;
+}
+
+.carousel-dot.active {
+  background-color: #fff;
+}
