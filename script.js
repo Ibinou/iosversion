08ac2ea0d694e6f64043a08c6855371c18152153
iBaDev <93536549+Ibinou@users.noreply.github.com>
2023-03-03 19:46:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,21 @@
-const carouselImages = document.querySelector(".carousel-images");
-const dotsContainer = document.querySelector(".carousel-dots");
-
-let scrollAmount = 0;
-const scrollStep = carouselImages.offsetWidth;
-
-carouselImages.addEventListener("scroll", () => {
-  const activeDot = document.querySelector(".carousel-dot.active");
-  if (activeDot) {
-    activeDot.classList.remove("active");
-  }
-  const dotIndex = Math.round(carouselImages.scrollLeft / scrollStep);
-  const dot = document.querySelector(`.carousel-dot[data-index="${dotIndex}"]`);
-  if (dot) {
-    dot.classList.add("active");
-  }
-});
-
-for (let i = 0; i < carouselImages.children.length; i++) {
-  const dot = document.createElement("div");
-  dot.classList.add("carousel-dot");
-  dot.setAttribute("data-index", i);
-  dot.addEventListener("click", () => {
-    carouselImages.scrollTo({
-      left: i * scrollStep,
-      behavior: "smooth"
-    });
-  });
-  dotsContainer.appendChild(dot);
+#apple-product {
+  background-color: #fff;
+  width: 300px;
+  height: 400px;
+  border: 2px solid #000;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 20px auto;
+  text-align: center;
 }
 
-let touchStartX = 0;
-let touchEndX = 0;
+#product-name {
+  margin-top: 0;
+}
 
-carouselImages.addEventListener("touchstart", e => {
-  touchStartX = e.touches[0].clientX;
-});
-
-carouselImages.addEventListener("touchend", e => {
-  touchEndX = e.changedTouches[0].clientX;
-  if (touchEndX < touchStartX) {
-    carouselImages.scrollBy({
-      left: scrollStep,
-      behavior: "smooth"
-    });
-  } else if (touchEndX > touchStartX) {
-    carouselImages.scrollBy({
-      left: -scrollStep,
-      behavior: "smooth"
-    });
-  }
-});
+#product-image {
+  width: 200px;
+  height: 200px;
+  margin-top: 20px;
+  border-radius: 10px;
+}
